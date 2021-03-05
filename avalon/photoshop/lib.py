b9175ea74e4d80e9379e0c1d858f422b09b3f83a
iLLiCiTiT <jakub.trllo@gmail.com>
2021-03-05 17:12:08 +0100

    modified photoshop's lib the same way as aftereffect's

--- avalon/photoshop/lib.py
+++ avalon/photoshop/lib.py
@@ -1,22 +1,30 @@
-import contextlib
-import subprocess
 import os
 import sys
+import contextlib
+import subprocess
 import queue
 import importlib
+import logging
+import functools
 import time
 import traceback
+
+from wsrpc_aiohttp import (
+    WebSocketRoute,
+    WebSocketAsync
+)
 
 from ..vendor.Qt import QtWidgets
 from ..tools import workfiles
 
-from pype.modules.websocket_server import WebSocketServer
-from pype.modules.websocket_server.stubs.photoshop_server_stub import (
-    PhotoshopServerStub
-)
+from avalon.tools.webserver.app import WebServerTool
+from .ws_stub import PhotoshopServerStub
 
 self = sys.modules[__name__]
 self.callback_queue = None
+
+log = logging.getLogger(__name__)
+log.setLevel(logging.DEBUG)
 
 
 def execute_in_main_thread(func_to_call_from_main_thread):
@@ -67,6 +75,63 @@ class ConnectionNotEstablishedYet(Exception):
     pass
 
 
+class PhotoshopRoute(WebSocketRoute):
+    """
+        One route, mimicking external application (like Harmony, etc).
+        All functions could be called from client.
+        'do_notify' function calls function on the client - mimicking
+            notification after long running job on the server or similar
+    """
+    instance = None
+
+    def init(self, **kwargs):
+        # Python __init__ must be return "self".
+        # This method might return anything.
+        log.debug("someone called Photoshop route")
+        self.instance = self
+        return kwargs
+
+    # server functions
+    async def ping(self):
+        log.debug("someone called Photoshop route ping")
+
+    # This method calls function on the client side
+    # client functions
+
+    async def read(self):
+        log.debug("photoshop.read client calls server server calls "
+                  "Photo client")
+        return await self.socket.call('Photoshop.read')
+
+    # panel routes for tools
+    async def creator_route(self):
+        self._tool_route("creator")
+
+    async def workfiles_route(self):
+        self._tool_route("workfiles")
+
+    async def loader_route(self):
+        self._tool_route("loader")
+
+    async def publish_route(self):
+        self._tool_route("publish")
+
+    async def sceneinventory_route(self):
+        self._tool_route("sceneinventory")
+
+    async def projectmanager_route(self):
+        self._tool_route("projectmanager")
+
+    def _tool_route(self, tool_name):
+        """The address accessed when clicking on the buttons."""
+        partial_method = functools.partial(show, tool_name)
+
+        execute_in_main_thread(partial_method)
+
+        # Required return statement.
+        return "nothing"
+
+
 def stub():
     """
         Convenience function to get server RPC stub to call methods directed
@@ -97,8 +162,14 @@ def launch(*subprocess_args):
     # Launch Photoshop and the websocket server.
     process = subprocess.Popen(subprocess_args, stdout=subprocess.PIPE)
 
-    websocket_server = WebSocketServer()
-    websocket_server.websocket_thread.start()
+    websocket_server = WebServerTool()
+    # Add Websocket route
+    websocket_server.add_route("*", "/ws/", WebSocketAsync)
+    # Add after effects route to websocket handler
+    WebSocketAsync.add_route(
+        PhotoshopRoute.__class__.__name__, PhotoshopRoute
+    )
+    websocket_server.start_server()
 
     while True:
         if process.poll() is not None:
