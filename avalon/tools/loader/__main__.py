29164b431ea86b31f50dd2266a0bbcdc655a4893
Petr Kalis <petr.kalis@gmail.com>
2021-03-22 18:52:51 +0100

    Hound

--- avalon/tools/loader/__main__.py
+++ avalon/tools/loader/__main__.py
@@ -2,10 +2,11 @@
 """
     Used for running 'avalon.tool.loader.__main__' as a module (-m), useful for
     debugging without need to start host.
-    
+
     Modify AVALON_MONGO accordingly
 """
 from . import cli
+
 
 def my_exception_hook(exctype, value, traceback):
     # Print the error and traceback
@@ -13,6 +14,7 @@ def my_exception_hook(exctype, value, traceback):
     # Call the normal Exception hook after
     sys._excepthook(exctype, value, traceback)
     sys.exit(1)
+
 
 if __name__ == '__main__':
     import os
